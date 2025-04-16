c1cf30355c50db007316d82c8a4349ab70b44969
Alfred Xavier C.T <Alfredxavierct@gmail.com>
2025-04-16 18:59:37 -0400

    Fix: Correct static folder name for deployment

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
@@ -7,19 +7,27 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname, "Public")));
 
 let messageHistory = [];
 
 io.on("connection", (socket) => {
-  // Generate a random username
+
   const username = "User_" + Math.floor(Math.random() * 1000);
   const color = getRandomColor();
 
   socket.username = username;
   socket.color = color;
 
-  socket.emit("init", { username, color, history: messageHistory });
+  
+  socket.emit("init", {
+    username,
+    color,
+    history: messageHistory,
+  });
+
+  
   socket.broadcast.emit("user-joined", username);
 
   socket.on("chat-message", (msg) => {
@@ -42,14 +50,17 @@ io.on("connection", (socket) => {
     });
   });
 
+
   socket.on("disconnect", () => {
     socket.broadcast.emit("user-left", socket.username);
   });
 });
 
+
 http.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
 
 function getRandomColor() {
   const colors = ["#f94144", "#f3722c", "#f9c74f", "#90be6d", "#43aa8b", "#577590"];
